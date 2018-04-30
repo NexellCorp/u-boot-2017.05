5a40a10eca820d6d28f8a143798b39608943d814
jhkim <jhkim@nexell.co.kr>
2018-04-30 15:04:48 +0900

    armv7: nxp3220 : add nxp3220/sip_s31nx base header
    This patch add nxp3220/sip_s31nx base header
    
    Change-Id: Iacfacb1d77db5702f479d2c81a51ca2a59f5fed1
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

---
+++ arch/arm/include/asm/arch-nxp3220/base.h
@@ -0,0 +1,11 @@
+/*
+ * (C) Copyright 2018 Nexell
+ * JunhHyun, Kim <jhkim@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#ifndef __ASM_ARCH_NXP3220_BASE_H
+#define __ASM_ARCH_NXP3220_BASE_H
+
+#endif /* __ASM_ARCH_NXP3220_BASE_H */
