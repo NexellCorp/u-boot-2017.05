0daee206661afdc898880307b73637acdf1a9d0f
jhkim <jhkim@nexell.co.kr>
2018-04-30 14:57:49 +0900

    armv7: nxp3220 : add armv7 nxp3220/sip_s31nx for Nexell SoC
    This patch add armv7 nxp3220/sip_s31nx for Nexell SoC
    
    Change-Id: Idc4d7db679222be714a50083ab3d3c82ef71b9de
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

---
+++ arch/arm/cpu/armv7/nxp3220/cpu.c
@@ -0,0 +1,26 @@
+/*
+ * (C) Copyright 2018 Nexell
+ * JongKeun, Choi <jkchoi@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#include <common.h>
+#include <command.h>
+#include <asm/system.h>
+#include <asm/cache.h>
+#include <asm/sections.h>
+#include <asm/io.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+void s_init(void)
+{
+}
+
+#if defined(CONFIG_DISPLAY_CPUINFO)
+int print_cpuinfo(void)
+{
+	return 0;
+}
+#endif
