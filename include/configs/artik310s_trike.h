0854bcfad0d45842e5138867d0d21f95a5854988
jhkim <jhkim@nexell.co.kr>
2018-05-11 13:46:29 +0900

    configs: trike: add artik310s_trike config
    This patch add artik310s_trike config for trike board based on artik310s
    
    Change-Id: I4b413b13d374539641ca518391ab08e6122c88d0
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

---
+++ include/configs/artik310s_trike.h
@@ -0,0 +1,16 @@
+/*
+ * (C) Copyright 2018 Nexell
+ * JungHyun, Kim <jhkim@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#ifndef __ARTIK310S_TRIKE_H__
+#define __ARTIK310S_TRIKE_H__
+
+#include "artik310s_common.h"
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"bootdelay=0\0"
+
+#endif
