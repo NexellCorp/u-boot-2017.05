b5be96d88499c2c890a759eae44537d628f66228
jhkim <jhkim@nexell.co.kr>
2018-05-11 13:37:34 +0900

    configs: artik310s: add artik310s_common config
    This patch add artik310s_common config.
    artik310s_common is common configuration options for all artik310s boards.
    
    Change-Id: Ice6552ce760de17d9c64abd8a3d15375be062ac0
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

---
+++ include/configs/artik310s_common.h
@@ -0,0 +1,46 @@
+/*
+ * (C) Copyright 2018 Nexell
+ * JungHyun, Kim <jhkim@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#ifndef __ARTIK310S_COMMON_H__
+#define __ARTIK310S_COMMON_H__
+
+#include <linux/sizes.h>
+
+/* dram 1 bank num */
+#define CONFIG_NR_DRAM_BANKS	1
+
+/* System memory Configuration */
+#define	CONFIG_SYS_TEXT_BASE	0x43C00000
+#define	CONFIG_SYS_INIT_SP_ADDR	CONFIG_SYS_TEXT_BASE
+#define CONFIG_SYS_MONITOR_BASE	CONFIG_SYS_TEXT_BASE
+#define CONFIG_SYS_SDRAM_BASE	0x40000000
+#define CONFIG_SYS_SDRAM_SIZE	0x70000000
+#define CONFIG_SYS_MALLOC_LEN	(64 * SZ_1M)
+
+/* kernel load address */
+#define CONFIG_SYS_LOAD_ADDR	0x48000000
+
+/* Console I/O Buffer Size  */
+#define CONFIG_SYS_CBSIZE	1024
+/* max number of command args   */
+#define CONFIG_SYS_MAXARGS	16
+
+/* For SD/MMC */
+#define CONFIG_BOUNCE_BUFFER
+
+/*
+ * Default environment organization
+ */
+#if !defined(CONFIG_ENV_IS_IN_MMC) && !defined(CONFIG_ENV_IS_IN_NAND) && \
+	!defined(CONFIG_ENV_IS_IN_FLASH) && !defined(CONFIG_ENV_IS_IN_EEPROM)
+	#define CONFIG_ENV_OFFSET	1024
+	#define CONFIG_ENV_SIZE		(4 * 1024) /* env size */
+	/* imls - list all images found in flash, default enable so disable */
+	#undef CONFIG_CMD_IMLS
+#endif
+
+#endif
