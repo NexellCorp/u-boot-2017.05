da3446bd0119b229720826b5706838da4b3fd987
Youngbok Park <ybpark@nexell.co.kr>
2018-05-18 15:27:03 +0900

    clk: nxp3220: add clk driver
    This patch add clk driver for nxp3220.
    
    Change-Id: I859f82b7425511ff9e9ede02df865c1b6e025664
    Signed-off-by: Youngbok Park <ybpark@nexell.co.kr>

---
+++ drivers/clk/nexell/clk-nxp3220-pll.c
@@ -0,0 +1,386 @@
+/*
+ * Nexell PLL helper functions for clock drivers.
+ * Copyright (C) 2018 Nexell .
+ * Author: Youngbok, Park <ybpark@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <dm.h>
+#include <errno.h>
+#include <clk-uclass.h>
+#include <asm/io.h>
+#include "clk-nxp3220.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#define PLL_LOCK_COUNT		0x200
+
+#define PLL_DIRTYFLAG		(1 << 1)
+#define PLL_RUN_CHANGE		(1 << 0)
+
+#define PMS_RATE(p, i) ((&p[i])->rate)
+#define PMS_P(p, i) ((&p[i])->P)
+#define PMS_M(p, i) ((&p[i])->M)
+#define PMS_S(p, i) ((&p[i])->S)
+#define PMS_K(p, i) ((&p[i])->K)
+
+#define PHY_BASEADDR_PLL0			0x27020000
+#define PHY_BASEADDR_PLL1			0x27030000
+#define PHY_BASEADDR_PLL_CPU			0x22070000
+#define PHY_BASEADDR_PLL_DDR0			0x23070000
+#define PHY_BASEADDR_PLL_DDR1			0x23080000
+
+#define PLL_LOCK_SHIFT		6
+#define PLL_CURST_SHIFT		8
+#define PLL_MUXSEL_SHIFT	3
+
+#define PLL_P_BITPOS 0
+#define PLL_M_BITPOS 16
+#define PLL_S_BITPOS 0
+#define PLL_K_BITPOS 16
+
+#define PLL_TYPE_2555 0
+#define PLL_TYPE_2651 1
+
+#define PLL_INIT(clk_num, addr)	\
+{				\
+	.reg = addr,		\
+}
+
+/* PLL(Phase Locked Loop) Register Map */
+struct nx_pll_reg {
+	unsigned int pll_ctrl;
+	unsigned int pll_dbg0;
+	unsigned int rsv0[2];
+	unsigned int pll_cnt0;
+	unsigned int pll_cnt1;
+	unsigned int pll_cnt2;
+	unsigned int pll_cnt3;
+	unsigned int pll_cfg0;
+	unsigned int rsv1[3];
+	unsigned int pll_cfg1;
+	unsigned int rsv2[3];
+	unsigned int pll_cfg2;
+	unsigned int rsv3[3];
+	unsigned int pll_ctrl1;
+	unsigned int rsv4[3];
+	unsigned int pll_lockinfo;
+};
+
+enum oscmux {
+	PLL_MUX_OSCCLK,
+	PLL_MUX_PLL_FOUT,
+};
+
+struct clk_priv {
+	struct nx_pll_reg *reg;
+	unsigned long rate;
+	int p, m, s, k;
+};
+
+struct cmu_platdata {
+	int initialized;
+};
+
+static unsigned long ref_clk = 24000000UL;
+
+struct pll_pms {
+	long rate;
+	int P;
+	int M;
+	int S;
+	int K;
+};
+
+static struct pll_pms pll_2555_pms[] = {
+	{ .rate = 2000000, .P = 3, .M = 250, .S = 0, .K = 0 },
+	{ .rate = 1900000, .P = 6, .M = 475, .S = 0, .K = 0 },
+	{ .rate = 1800000, .P = 3, .M = 225, .S = 0, .K = 0 },
+	{ .rate = 1700000, .P = 6, .M = 425, .S = 0, .K = 0 },
+	{ .rate = 1600000, .P = 3, .M = 200, .S = 0, .K = 0 },
+	{ .rate = 1500000, .P = 4, .M = 250, .S = 0, .K = 0 },
+	{ .rate = 1400000, .P = 3, .M = 175, .S = 0, .K = 0 },
+	{ .rate = 1300000, .P = 6, .M = 325, .S = 0, .K = 0 },
+	{ .rate = 1200000, .P = 3, .M = 300, .S = 1, .K = 0 },
+	{ .rate = 1100000, .P = 3, .M = 275, .S = 1, .K = 0 },
+	{ .rate = 1000000, .P = 3, .M = 250, .S = 1, .K = 0 },
+	{ .rate =  900000, .P = 3, .M = 225, .S = 1, .K = 0 },
+	{ .rate =  800000, .P = 3, .M = 200, .S = 1, .K = 0 },
+	{ .rate =  700000, .P = 3, .M = 175, .S = 1, .K = 0 },
+	{ .rate =  600000, .P = 3, .M = 200, .S = 2, .K = 0 },
+	{ .rate =  500000, .P = 3, .M = 250, .S = 2, .K = 0 },
+	{ .rate =  400000, .P = 3, .M = 200, .S = 2, .K = 0 },
+	{ .rate =  300000, .P = 3, .M = 300, .S = 3, .K = 0 },
+	{ .rate =  200000, .P = 3, .M = 200, .S = 3, .K = 0 },
+	{ .rate =  100000, .P = 3, .M = 200, .S = 4, .K = 0 },
+};
+
+static struct pll_pms pll_2651_pms[] = {
+	{ .rate = 1000000, .P = 3, .M = 250, .S = 0, .K = 0 },
+	{ .rate =  900000, .P = 3, .M = 225, .S = 0, .K = 0 },
+	{ .rate =  800000, .P = 3, .M = 200, .S = 0, .K = 0 },
+	{ .rate =  700000, .P = 3, .M = 175, .S = 0, .K = 0 },
+	{ .rate =  600000, .P = 3, .M = 300, .S = 1, .K = 0 },
+	{ .rate =  500000, .P = 3, .M = 250, .S = 1, .K = 0 },
+	{ .rate =  400000, .P = 3, .M = 200, .S = 1, .K = 0 },
+	{ .rate =  300000, .P = 3, .M = 200, .S = 2, .K = 0 },
+	{ .rate =  200000, .P = 3, .M = 200, .S = 2, .K = 0 },
+	{ .rate =  100000, .P = 3, .M = 200, .S = 3, .K = 0 },
+};
+
+static struct clk_priv cmu_pll[] = {
+	PLL_INIT(0, (struct nx_pll_reg *)PHY_BASEADDR_PLL0),
+	PLL_INIT(1, (struct nx_pll_reg *)PHY_BASEADDR_PLL1),
+	PLL_INIT(2, (struct nx_pll_reg *)PHY_BASEADDR_PLL_CPU),
+	PLL_INIT(3, (struct nx_pll_reg *)PHY_BASEADDR_PLL_DDR0),
+	PLL_INIT(4, (struct nx_pll_reg *)PHY_BASEADDR_PLL_DDR1)
+};
+
+static int get_pll_baseaddr(int index)
+{
+	const unsigned int baseaddr[5] = {
+		PHY_BASEADDR_PLL0,
+		PHY_BASEADDR_PLL1,
+		PHY_BASEADDR_PLL_CPU,
+		PHY_BASEADDR_PLL_DDR0,
+		PHY_BASEADDR_PLL_DDR1
+	};
+
+	return baseaddr[index];
+}
+
+static int get_use_ddr_pll(void)
+{
+	struct clk_cmu_reg *reg =
+		(struct clk_cmu_reg *)PHY_BASEADDR_CMU_DDR_MODULE;
+
+	return readl(&reg->grp_clk_src) + 3;
+}
+
+static unsigned long pll_round_rate(int pllno, unsigned long rate, int *p,
+		int *m, int *s, int *k)
+{
+	struct pll_pms *pms = NULL;
+	int len = 0, l = 0;
+	long freq = 0;
+
+	rate /= 1000;
+
+	switch (pllno) {
+	case 0:
+	case 2:
+	case 3:
+		pms = pll_2555_pms;
+		len = ARRAY_SIZE(pll_2555_pms);
+		break;
+	case 1:
+	case 4:
+		pms = pll_2651_pms;
+		len = ARRAY_SIZE(pll_2651_pms);
+		break;
+	}
+
+	for (l = 0; l < len; l++) {
+		if (freq >= PMS_RATE(pms, l))
+			break;
+	}
+
+	if (p)
+		*p = PMS_P(pms, l);
+	if (m)
+		*m = PMS_M(pms, l);
+	if (s)
+		*s = PMS_S(pms, l);
+	if (k)
+		*k = PMS_K(pms, l);
+
+	printf("real %ld Khz, P=%d ,M=%3d, S=%d K=%d\n", PMS_RATE(pms, l),
+	       PMS_P(pms, l), PMS_M(pms, l), PMS_S(pms, l), PMS_K(pms, l));
+
+	return PMS_RATE(pms, l) * 1000;
+}
+
+static void set_oscmux(unsigned int pll_num, unsigned int muxsel)
+{
+	struct nx_pll_reg *base
+		= (struct nx_pll_reg *)get_pll_baseaddr(pll_num);
+	unsigned int tmp;
+
+	tmp = readl(&base->pll_ctrl);
+	writel(tmp | (muxsel << 3), &base->pll_ctrl);
+}
+
+static int get_pll_type(int pllno)
+{
+	if (pllno == 1 || pllno == 4)
+		return PLL_TYPE_2651;
+
+	return PLL_TYPE_2555;
+}
+
+static int check_pll_lock(int pll_num)
+{
+	struct nx_pll_reg *base
+		= (struct nx_pll_reg *)get_pll_baseaddr(pll_num);
+
+	unsigned int st_count, st_check;
+	unsigned int cur_st, lock, mux, tmp;
+
+	st_count = readl(&base->pll_dbg0);
+	tmp = readl(&base->pll_ctrl);
+	lock = ((tmp >> PLL_LOCK_SHIFT) & 0x1);
+	cur_st = (tmp >> PLL_CURST_SHIFT) & 0x1F;
+
+	mux = (tmp >> PLL_MUXSEL_SHIFT) & 0x1;
+
+	if (mux)
+		st_check = 1;
+	else
+		st_check = (st_count > PLL_LOCK_COUNT);
+
+	if (((cur_st == 1) && (lock == 1)) && st_check)
+		return true;
+	else
+		return false;
+}
+
+static int clock_is_stable(int pll_num)
+{
+	int timeout = 0x200000;
+
+	while (check_pll_lock(pll_num) == false) {
+		if (timeout-- <= 0)
+			return false;
+	}
+
+	return true;
+}
+
+static int set_pll_rate(int pllno, int p, int m, int s, int k)
+{
+	struct nx_pll_reg *base;
+	unsigned int tmp;
+
+	base = (struct nx_pll_reg *)get_pll_baseaddr(pllno);
+
+	if (clock_is_stable(pllno))
+		return -EINVAL;
+
+	set_oscmux(pllno, PLL_MUX_PLL_FOUT);
+	writel(p << PLL_P_BITPOS | m << PLL_M_BITPOS, &base->pll_cfg1);
+	writel(s << PLL_S_BITPOS | k << PLL_K_BITPOS, &base->pll_cfg2);
+	tmp = readl(&base->pll_ctrl);
+	writel(tmp | PLL_DIRTYFLAG | PLL_RUN_CHANGE, &base->pll_ctrl);
+	clock_is_stable(pllno);
+
+	return 0;
+}
+
+static inline unsigned int pll_rate(unsigned int pllno, unsigned int xtal)
+{
+	struct nx_pll_reg *reg =
+		(struct nx_pll_reg *)get_pll_baseaddr(pllno);
+	unsigned int val, val1, nP, nM, nS, nK;
+	unsigned int temp = 0;
+
+	val = reg->pll_cfg1;
+	val1 = reg->pll_cfg2;
+	xtal /= 1000; /* Unit Khz */
+
+	nP = val & 0x03F;
+	nM = (val >> 16) & 0x3FF;
+	nS = (val1 >> 0) & 0x0FF;
+	nK = (val1 >> 16) & 0xFFFF;
+
+	if (get_pll_type(pllno) && nK)
+		temp = ((((nK * 1000) / 65536) * xtal) / nP) >> nS;
+
+	return (unsigned int)((((nM * xtal) / nP) >> nS) * 1000) + temp;
+}
+
+static ulong nx_pll_get_rate(struct clk *clk)
+{
+	ulong rate;
+
+	rate = pll_rate(clk->id, ref_clk);
+
+	if (clk->id == 1 || clk->id >= 3)
+		return rate/2;
+
+	return rate;
+}
+
+static ulong nx_pll_set_rate(int pllno, ulong freq)
+{
+	int p = 0, m = 0, s = 0, k = 0;
+	ulong rate = 0;
+
+	if (pllno == CPU_PLL) {
+		printf(" This channel(%d) is in used for CPU\n", pllno);
+		return -1;
+	}
+
+	if (pllno == get_use_ddr_pll()) {
+		printf(" This channel(%d) is in used for DDR Memory\n", pllno);
+		return -1;
+	}
+
+	rate = pll_round_rate(pllno, freq, &p, &m, &s, &k);
+
+	set_pll_rate(pllno, p, m, s, k);
+
+	cmu_pll[pllno].p = p;
+	cmu_pll[pllno].m = m;
+	cmu_pll[pllno].s = s;
+	cmu_pll[pllno].k = k;
+
+	return rate;
+}
+
+int nx_pll_parse_dt(struct udevice *dev)
+{
+	const void *blob = gd->fdt_blob;
+	int node = dev_of_offset(dev);
+	int length, i;
+	const fdt32_t *list;
+	unsigned int init[5][2];
+
+	list = fdt_getprop(blob, node, "frequency", &length);
+	if (!list)
+		return -1;
+
+	length /= sizeof(*list);
+	for (i = 0; i < length/2; i++) {
+		init[i][0] = fdt32_to_cpu(*list++);
+		init[i][1] = fdt32_to_cpu(*list++);
+		nx_pll_set_rate(init[i][0], init[i][1]);
+	}
+
+	return 0;
+}
+
+static int nx_pll_probe(struct udevice *dev)
+{
+	return nx_pll_parse_dt(dev);
+}
+
+static struct clk_ops nx_pll_ops = {
+	.get_rate = nx_pll_get_rate,
+};
+
+static const struct udevice_id nx_pll_compat[] = {
+	{ .compatible = "nexell,nxp3220-clock-pll" },
+	{ }
+};
+
+U_BOOT_DRIVER(nx_pll) = {
+	.name = "nexell-clock-pll",
+	.id = UCLASS_CLK,
+	.of_match = nx_pll_compat,
+	.ops = &nx_pll_ops,
+	.probe = nx_pll_probe,
+	.priv_auto_alloc_size = sizeof(struct clk_priv),
+	.flags = DM_FLAG_PRE_RELOC,
+};
---
+++ drivers/clk/nexell/clk-nxp3220.c
@@ -0,0 +1,551 @@
+/*
+ * Nexell PLL helper functions for clock drivers.
+ * Copyright (C) 2018 Nexell .
+ * Youngbok Park<ybpark@nxell.co.kr>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <dm.h>
+#include <errno.h>
+#include <clk-uclass.h>
+#include <asm/io.h>
+#include "clk-nxp3220.h"
+#include "clk-init-nxp3220.c"
+
+#define SRC_MAX_DIV 256
+#define SYS_MAX_DIV 256
+
+#define T_MAX_DIV SRC_MAX_DIV * SYS_MAX_DIV;
+
+#define PLL_NUM 5
+
+DECLARE_GLOBAL_DATA_PTR;
+
+struct nx_clk_div {
+	int mux;
+	int div_s;
+	int div_o;
+};
+
+struct nx_cmu_priv {
+	struct clk_cmu_dev *cmus;
+	struct clk_cmu_dev *src_cmu;
+	int size;
+	int p_size;
+};
+
+static unsigned long plls[] = { 400000000,
+			0,
+			0,
+			0,
+			0};
+
+static int nx_get_min_pll(void)
+{
+	int i;
+	int min = 0;
+
+	for (i = 1; i < PLL_NUM; i++) {
+		if (plls[i] == 0)
+			continue;
+		if (plls[i] < plls[min])
+			min = i;
+	}
+
+	return min;
+}
+
+static int get_max_pll(void)
+{
+	int i;
+	int max = 0;
+
+	for (i = 1; i < PLL_NUM; i++) {
+		if (plls[i] == 0)
+			continue;
+		if (plls[i] > plls[max])
+			max = i;
+	}
+
+	return max;
+}
+
+static int nx_calc_divisor(unsigned long req, struct nx_clk_div *cdiv)
+{
+	int div_c_max = SYS_MAX_DIV;
+	int div_max = T_MAX_DIV;
+	int div = 0; int mux = 0; int dlt = 0;
+	int div0 = 0, div1 = 0;
+	int v, m, i, d;
+
+	for (i = 0; i < PLL_NUM; i++) {
+		if (plls[i] == 0)
+			continue;
+		v = plls[i] / req;
+		m = plls[i] % req;
+
+		/* under the min divider */
+		if (v == 0) {
+			div = 0;
+			continue;
+		}
+
+		if (v > div_max) {
+			div = div_max+1;
+			continue;
+		}
+
+		if (m == 0) {
+			mux = i; div = v; dlt = m;
+			break;
+		}
+
+		if (dlt == 0 || m < dlt) {
+			mux = i; div = v; dlt = m;
+		}
+	}
+
+	if (div > div_max || div == 0) {
+		if (div == 0) {
+			mux = get_max_pll();
+			div0 = 1;
+			div1 = 1;
+		} else {
+			mux = nx_get_min_pll();
+			div0 = SRC_MAX_DIV;
+			div1 = SYS_MAX_DIV;
+		}
+
+		cdiv->mux = mux;
+		cdiv->div_s = div0;
+		cdiv->div_o = div1;
+		return 0;
+	}
+
+	for (d = 1; d < 255; d++) {
+		v = div / d;
+		m = div % d;
+
+		if (v > div_c_max)
+			continue;
+
+		if (m == 0) {
+			div0 = d;
+			div1 = v;
+			break;
+		}
+		if (dlt == 0 || m < dlt) {
+			div0 = d; div1 = v;
+		}
+	}
+	cdiv->mux = mux;
+	cdiv->div_s = div0;
+	cdiv->div_o = div1;
+	return 0;
+}
+
+static struct clk_cmu_dev *nx_get_clk_priv(struct nx_cmu_priv *priv, int id)
+{
+	int i;
+
+	for (i = 0; priv->size; i++) {
+		if (priv->cmus[i].id == id)
+			return (struct clk_cmu_dev *)&priv->cmus[i].reg;
+	}
+	return (void *)NULL;
+}
+
+static struct clk_cmu_dev *nx_get_clk_src_priv(struct nx_cmu_priv *priv, int id)
+{
+	int i;
+
+	for (i = 0; priv->p_size; i++) {
+		if (priv->src_cmu[i].id == id)
+			return (struct clk_cmu_dev *)&priv->src_cmu[i].reg;
+	}
+	return (void *)NULL;
+}
+
+static int clk_grp_enable(struct nx_cmu_priv *priv, unsigned int id)
+{
+	struct clk_cmu_dev *sys = nx_get_clk_priv(priv, id);
+	struct clk_cmu_dev *src = nx_get_clk_priv(priv, sys->p_id);
+	unsigned int reg_idx = sys->clkenbit / 32;
+	unsigned int reg_bit = sys->clkenbit % 32;
+	unsigned int src_reg_idx = src->clkenbit / 32;
+	unsigned int src_bit = src->clkenbit % 32;
+
+	if (sys->nc) {
+		printf("CMU %d is not en/disable\n", id);
+		return -EINVAL;
+	}
+	writel(1 << reg_bit, &sys->reg->grp_clkenb[reg_idx]);
+	writel(1 << src_bit, &src->reg->grp_clkenb[src_reg_idx]);
+
+	return 0;
+}
+
+static int nx_clk_enable(struct clk *clk)
+{
+	struct nx_cmu_priv *priv = dev_get_priv(clk->dev);
+	clk_grp_enable(priv, clk->id);
+
+	return 0;
+}
+
+static int nx_clk_disable(struct clk *clk)
+{
+	struct nx_cmu_priv *priv = dev_get_priv(clk->dev);
+	struct clk_cmu_dev *sys = nx_get_clk_priv(priv, clk->id);
+	struct clk_cmu_dev *src = nx_get_clk_priv(priv, sys->p_id);
+	unsigned int reg_idx = sys->clkenbit / 32;
+	unsigned int reg_bit = sys->clkenbit % 32;
+	unsigned int src_reg_idx = src->clkenbit / 32;
+	unsigned int src_bit = src->clkenbit % 32;
+
+	if (sys->nc) {
+		printf("CMU %ld is not disable\n", clk->id);
+		return -EINVAL;
+	}
+	writel(1<<reg_bit, &sys->reg->grp_clkenb_clr[reg_idx]);
+	writel(1<<src_bit, &src->reg->grp_clkenb_clr[src_reg_idx]);
+
+	return 0;
+}
+
+static ulong set_rate(struct clk_cmu_dev *sys, struct clk_cmu_dev *src,
+			unsigned long freq)
+{
+	unsigned long cal_freq;
+	struct nx_clk_div cdiv;
+
+	nx_calc_divisor(freq, &cdiv);
+
+	writel((cdiv.mux - 1) & 0xFFFF, &src->reg->grp_clk_src);
+	writel((cdiv.div_s - 1) & 0xFFFF, &src->reg->divider[0]);
+	writel((cdiv.div_o - 1) & 0xFFFF, &sys->reg->divider[0]);
+
+	cal_freq = plls[cdiv.mux] / cdiv.div_s / cdiv.div_o;
+	sys->c_freq = cal_freq;
+
+	return cal_freq;
+}
+
+static ulong nx_set_rate(struct nx_cmu_priv *priv , int id, unsigned long freq)
+{
+	struct clk_cmu_dev *sys = nx_get_clk_priv(priv, id);
+	struct clk_cmu_dev *par;
+	struct clk_cmu_dev *src;
+	unsigned long cal_freq;
+	int div, div_index;
+
+	switch (sys->type) {
+	case CMU_TYPE_MAINDIV:
+		switch (sys->cmu_id) {
+		case CMU_NAME_SRC:
+			break;
+		case CMU_NAME_SYS:
+		case CMU_NAME_MM:
+		case CMU_NAME_USB:
+			src = nx_get_clk_src_priv(priv, sys->p_id);
+			cal_freq = set_rate(sys, src, freq);
+			return cal_freq;
+			break;
+		}
+		break;
+	case CMU_TYPE_SUBDIV0:
+	case CMU_TYPE_SUBDIV1:
+	case CMU_TYPE_SUBDIV2:
+		src = nx_get_clk_priv(priv, sys->p_id);
+		div_index = sys->type - CMU_TYPE_SUBDIV0;
+		if (!src->c_freq || src->c_freq < freq) {
+			div = 1;
+			cal_freq = src->c_freq;
+		} else {
+			div = src->c_freq / freq;
+			cal_freq = src->c_freq / div;
+			sys->c_freq = cal_freq;
+		}
+		writel(div - 1, &sys->reg->divider[div_index]);
+		return cal_freq;
+		break;
+	case CMU_TYPE_ONLYGATE:
+		par = nx_get_clk_priv(priv, sys->p_id);
+		src = nx_get_clk_src_priv(priv, par->p_id);
+		if (!src->c_freq)
+			set_rate(par, src, freq);
+		else
+			sys->c_freq = src->c_freq;
+		return sys->c_freq;
+		break;
+	}
+	return 0;
+}
+
+static ulong nx_cmu_set_rate(struct clk *clk, unsigned long freq)
+{
+	struct nx_cmu_priv *priv = dev_get_priv(clk->dev);
+
+	return nx_set_rate(priv, clk->id, freq);
+}
+
+static ulong nx_cmu_get_rate(struct clk *clk)
+{
+	struct nx_cmu_priv *priv = dev_get_priv(clk->dev);
+	struct clk_cmu_dev *sys = nx_get_clk_priv(priv, clk->id);
+	unsigned long rate = 0;
+
+	switch (sys->type) {
+	case CMU_TYPE_MAINDIV:
+		if (sys->cmu_id != CMU_NAME_SRC)
+			rate = sys->c_freq;
+		break;
+	case CMU_TYPE_SUBDIV0:
+	case CMU_TYPE_SUBDIV1:
+	case CMU_TYPE_SUBDIV2:
+		rate = sys->c_freq;
+		break;
+	case CMU_TYPE_ONLYGATE:
+		rate = sys->c_freq;
+		break;
+	}
+	return rate;
+}
+
+static int nx_get_use_ddr_pll(void)
+{
+	struct clk_cmu_reg *reg =
+		(struct clk_cmu_reg *)PHY_BASEADDR_CMU_DDR_MODULE;
+
+	return readl(&reg->grp_clk_src) + 3;
+}
+
+static int nx_get_pll_rate(struct udevice *dev)
+{
+	struct clk pll;
+	int i, ddr = nx_get_use_ddr_pll();
+
+	clk_get_by_index(dev, 0, &pll);
+
+	for (i = 0; i < PLL_NUM; i++) {
+		if (i == ddr || i == CPU_PLL)
+			continue;
+		pll.id = i;
+		plls[i] = clk_get_rate(&pll);
+		if (i == 3 || i == 4)
+			plls[i] = plls[i]/2;
+	}
+
+	return 0;
+}
+
+static int nx_parse_cmu_dt(struct udevice *dev)
+{
+	const void *blob = gd->fdt_blob;
+	struct nx_cmu_priv *priv = dev_get_priv(dev);
+	int node = dev_of_offset(dev);
+	const fdt32_t *list;
+	unsigned int init[128][2];
+	int length, i, en;
+
+	list = fdt_getprop(blob, node, "bus-init-frequency", &length);
+	if (list) {
+		length /= sizeof(*list);
+		for (i = 0; i < length/3; i++) {
+			init[i][0] = fdt32_to_cpu(*list++);
+			init[i][1] = fdt32_to_cpu(*list++);
+			en = fdt32_to_cpu(*list++);
+
+			nx_set_rate(priv, init[i][0], init[i][1]);
+			if (en)
+				clk_grp_enable(priv, init[i][0]);
+		}
+	}
+
+	list = fdt_getprop(blob, node, "clk-init-frequency", &length);
+	if (list) {
+		length /= sizeof(*list);
+		for (i = 0; i < length/3; i++) {
+			init[i][0] = fdt32_to_cpu(*list++);
+			init[i][1] = fdt32_to_cpu(*list++);
+			en = fdt32_to_cpu(*list++);
+
+			nx_set_rate(priv, init[i][0], init[i][1]);
+			if (en)
+				clk_grp_enable(priv, init[i][0]);
+		}
+	}
+
+	return 0;
+}
+
+static int clk_cmu_mm_probe(struct udevice *dev)
+{
+	struct nx_cmu_priv *mm_priv = dev_get_priv(dev);
+	unsigned int reg = devfdt_get_addr(dev);
+	struct clk src;
+	int i;
+
+	mm_priv->cmus = (struct clk_cmu_dev *)&cmu_mm;
+	mm_priv->size = ARRAY_SIZE(cmu_mm);
+	mm_priv->src_cmu = (struct clk_cmu_dev *)&cmu_src;
+	mm_priv->p_size = ARRAY_SIZE(cmu_src);
+
+	for (i = 0; i < mm_priv->size; i++)
+		mm_priv->cmus[i].reg = (void *)mm_priv->cmus[i].reg + reg;
+
+	clk_get_by_index(dev, 0, &src);
+	nx_parse_cmu_dt(dev);
+
+	return 0;
+}
+
+static int clk_cmu_usb_probe(struct udevice *dev)
+{
+	struct nx_cmu_priv *usb_priv = dev_get_priv(dev);
+	unsigned int reg = devfdt_get_addr(dev);
+	struct clk src;
+	int i;
+
+	usb_priv->cmus = (struct clk_cmu_dev *)&cmu_usb;
+	usb_priv->size = ARRAY_SIZE(cmu_usb);
+	usb_priv->src_cmu = (struct clk_cmu_dev *)&cmu_src;
+	usb_priv->p_size = ARRAY_SIZE(cmu_src);
+
+	for (i = 0; i < usb_priv->size; i++)
+		usb_priv->cmus[i].reg =
+			(void *)usb_priv->cmus[i].reg + reg;
+
+	clk_get_by_index(dev, 0, &src);
+	nx_parse_cmu_dt(dev);
+
+	return 0;
+}
+
+static int clk_cmu_sys_probe(struct udevice *dev)
+{
+	struct nx_cmu_priv *sys_priv = dev_get_priv(dev);
+	unsigned int reg = devfdt_get_addr(dev);
+	struct clk src;
+	int i;
+
+	sys_priv->cmus = (struct clk_cmu_dev *)&cmu_sys;
+	sys_priv->size = ARRAY_SIZE(cmu_sys);
+	sys_priv->src_cmu = (struct clk_cmu_dev *)&cmu_src;
+	sys_priv->p_size = ARRAY_SIZE(cmu_src);
+
+	for (i = 0; i < sys_priv->size; i++)
+		sys_priv->cmus[i].reg =	(void *)sys_priv->cmus[i].reg + reg;
+
+	clk_get_by_index(dev, 0, &src);
+	nx_parse_cmu_dt(dev);
+
+	return 0;
+}
+
+static int nx_clk_src_probe(struct udevice *dev)
+{
+	struct nx_cmu_priv *src_priv = dev_get_priv(dev);
+	unsigned int reg = devfdt_get_addr(dev);
+	int i;
+
+	src_priv->cmus = (struct clk_cmu_dev *)&cmu_src;
+	src_priv->size = ARRAY_SIZE(cmu_src);
+
+	for (i = 0; i < src_priv->size; i++)
+		src_priv->cmus[i].reg = (void *)src_priv->cmus[i].reg + reg;
+
+	nx_get_pll_rate(dev);
+
+	return 0;
+}
+
+static struct clk_ops nx_cmu_src_ops = {
+	.get_rate	= nx_cmu_get_rate,
+	.set_rate	= nx_cmu_set_rate,
+	.enable		= nx_clk_enable,
+	.disable	= nx_clk_disable,
+};
+
+static const struct udevice_id nx_cmu_src_compat[] = {
+	{ .compatible = "nexell,nxp3220-cmu-src" },
+	{ }
+};
+
+U_BOOT_DRIVER(nx_cmu_src) = {
+	.name = "nx-cmu-src",
+	.id = UCLASS_CLK,
+	.of_match = nx_cmu_src_compat,
+	.probe = nx_clk_src_probe,
+	.ops = &nx_cmu_src_ops,
+	.flags = DM_FLAG_PRE_RELOC,
+	.priv_auto_alloc_size = sizeof(struct nx_cmu_priv),
+};
+
+static struct clk_ops nx_cmu_sys_ops = {
+	.get_rate	= nx_cmu_get_rate,
+	.set_rate	= nx_cmu_set_rate,
+	.enable		= nx_clk_enable,
+	.disable	= nx_clk_disable,
+};
+
+static const struct udevice_id nx_cmu_sys_compat[] = {
+	{ .compatible = "nexell,nxp3220-cmu-sys" },
+	{ }
+};
+
+U_BOOT_DRIVER(nx_cmu_sys) = {
+	.name = "nx-cmu-sys",
+	.id = UCLASS_CLK,
+	.of_match = nx_cmu_sys_compat,
+	.probe = clk_cmu_sys_probe,
+	.ops = &nx_cmu_sys_ops,
+	.flags = DM_FLAG_PRE_RELOC,
+	.priv_auto_alloc_size = sizeof(struct nx_cmu_priv),
+};
+
+static struct clk_ops nx_cmu_mm_ops = {
+	.get_rate	= nx_cmu_get_rate,
+	.set_rate	= nx_cmu_set_rate,
+	.enable		= nx_clk_enable,
+	.disable	= nx_clk_disable,
+};
+
+static const struct udevice_id nx_cmu_mm_compat[] = {
+	{ .compatible = "nexell,nxp3220-cmu-mm" },
+	{ }
+};
+
+U_BOOT_DRIVER(nx_cmu_mm) = {
+	.name = "nx-cmu-mm",
+	.id = UCLASS_CLK,
+	.of_match = nx_cmu_mm_compat,
+	.probe = clk_cmu_mm_probe,
+	.ops = &nx_cmu_mm_ops,
+	.flags = DM_FLAG_PRE_RELOC,
+	.priv_auto_alloc_size = sizeof(struct nx_cmu_priv),
+};
+
+static struct clk_ops nx_cmu_usb_ops = {
+	.get_rate	= nx_cmu_get_rate,
+	.set_rate	= nx_cmu_set_rate,
+	.enable		= nx_clk_enable,
+	.disable	= nx_clk_disable,
+};
+
+static const struct udevice_id nx_cmu_usb_compat[] = {
+	{ .compatible = "nexell,nxp3220-cmu-usb" },
+	{ }
+};
+
+U_BOOT_DRIVER(nx_cmu_usb) = {
+	.name = "nx-cmu-usb",
+	.id = UCLASS_CLK,
+	.of_match = nx_cmu_usb_compat,
+	.probe = clk_cmu_usb_probe,
+	.ops = &nx_cmu_usb_ops,
+	.flags = DM_FLAG_PRE_RELOC,
+	.priv_auto_alloc_size = sizeof(struct nx_cmu_priv),
+};
