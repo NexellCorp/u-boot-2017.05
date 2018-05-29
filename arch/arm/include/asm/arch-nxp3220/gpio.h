0caa7f75809892415abb7f477e5e4ecfe4ba2e97
Bon-gyu, KOO <freestyle@nexell.co.kr>
2018-05-29 14:43:00 +0900

    gpio: nexell: Add the GPIO driver support
    This patch provides Nexell SoC GPIO driver.
    
    Change-Id: I966ff0d944f86debebde270cf3f28b1c5047334c
    Signed-off-by: Bon-gyu, KOO <freestyle@nexell.co.kr>

---
+++ arch/arm/include/asm/arch-nxp3220/gpio.h
@@ -0,0 +1,11 @@
+/*
+ * (C) Copyright 2018 Nexell
+ * Bongyu, KOO <freestyle@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#ifndef __ASM_ARCH_NEXELL_GPIO_H
+#define __ASM_ARCH_NEXELL_GPIO_H
+
+#endif /* __ASM_ARCH_NEXELL_GPIO_H */
