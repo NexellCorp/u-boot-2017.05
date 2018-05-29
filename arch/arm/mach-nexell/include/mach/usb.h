dd2240cc82066b761edec5778c55d583c3f042ed
jhkim <jhkim@nexell.co.kr>
2018-05-29 18:08:10 +0900

    usb: nxp3220 usb otg phy: Add usb otg phy driver for nxp3220
    This patch adds usb otg phy driver for nxp3220
    
    Change-Id: I00be693b6ac5430e87216cac9adc84b00d107d6f
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

---
+++ arch/arm/mach-nexell/include/mach/usb.h
@@ -0,0 +1,18 @@
+/*
+ * (C) Copyright 2018 Nexell
+ * SangJong, Han <hans@nexell.co.kr>
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+#ifndef __NEXELL_USB_H
+#define __NEXELL_USB_H
+
+struct nx_otg_phy {
+	struct udevice *dev;
+	void __iomem *addr;
+};
+
+void nx_otg_phy_init(struct nx_otg_phy *phy);
+void nx_otg_phy_off(struct nx_otg_phy *phy);
+
+#endif
