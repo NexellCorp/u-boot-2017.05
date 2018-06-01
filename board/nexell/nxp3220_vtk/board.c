c3862dd24a73cca8f4ed65c09d6c26628fc0f5b7
jhkim <jhkim@nexell.co.kr>
2018-06-01 16:37:44 +0900

    board: nexell: add nxp3220_vtk board
    This patch adds nxp3220_vtk board based on nxp3220
    
    Change-Id: I3ac07076e2ac4b98d861325b0e46820ae74c6143
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

---
+++ board/nexell/nxp3220_vtk/board.c
@@ -0,0 +1,16 @@
+/*
+ * (C) Copyright 2018 Nexell
+ * JungHyun, Kim <jhkim@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#include <config.h>
+#include <common.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int board_init(void)
+{
+	return 0;
+}
