086506930440c681ce7441be21cf07e0a043bf99
jhkim <jhkim@nexell.co.kr>
2018-06-01 18:10:40 +0900

    configs: trike: add nxp3220_vtk config
    This patch add nxp3220_vtk config for vtk board based on nxp3220
    
    Change-Id: I2197b017cdecd42adaec65f8d1f7b7ea3e82b402
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

---
+++ include/configs/nxp3220_vtk.h
@@ -0,0 +1,16 @@
+/*
+ * (C) Copyright 2018 Nexell
+ * JungHyun, Kim <jhkim@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#ifndef __NXP3220_VTK_H__
+#define __NXP3220_VTK_H__
+
+#include "nxp3220_common.h"
+
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"bootdelay=0\0"
+
+#endif
