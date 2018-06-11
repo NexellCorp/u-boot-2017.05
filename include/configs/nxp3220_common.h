01f0a9efef4a4a2cf20c401707a2724e681ebe63
jhkim <jhkim@nexell.co.kr>
2018-06-11 11:44:57 +0900

    configs: nxp3220: add nxp3220_common config
    This patch add nxp3220_common config.
    nxp3220_common is common configuration options for all nxp3220 boards.
    
    Change-Id: Idfc2b0a75ff9d410742a1916db9a23ac738db599
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

---
+++ include/configs/nxp3220_common.h
@@ -0,0 +1,32 @@
+/*
+ * (C) Copyright 2018 Nexell
+ * JungHyun, Kim <jhkim@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#ifndef __NXP3220_COMMON_H__
+#define __NXP3220_COMMON_H__
+
+#include <linux/sizes.h>
+
+/* dram 1 bank num */
+#define CONFIG_NR_DRAM_BANKS	1
+
+/* Console I/O Buffer Size  */
+#define CONFIG_SYS_CBSIZE	1024
+/* max number of command args   */
+#define CONFIG_SYS_MAXARGS	16
+
+/*
+ * Default environment organization
+ */
+#if !defined(CONFIG_ENV_IS_IN_MMC) && !defined(CONFIG_ENV_IS_IN_NAND) && \
+	!defined(CONFIG_ENV_IS_IN_FLASH) && !defined(CONFIG_ENV_IS_IN_EEPROM)
+	#define CONFIG_ENV_OFFSET	1024
+	#define CONFIG_ENV_SIZE		(16 * 1024) /* env size */
+	/* imls - list all images found in flash, default enable so disable */
+	#undef CONFIG_CMD_IMLS
+#endif
+
+#endif
