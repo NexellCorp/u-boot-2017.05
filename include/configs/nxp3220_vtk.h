cb824c8cdd075954f5497792667fc7460ba90d6c
jhkim <jhkim@nexell.co.kr>
2018-06-11 11:46:12 +0900

    configs: vtk: add nxp3220_vtk config
    This patch add nxp3220_vtk config for vtk board based on nxp3220
    
    Change-Id: If05daa9692e3721ade9478a0d1ae0f472987a86a
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

---
+++ include/configs/nxp3220_vtk.h
@@ -0,0 +1,28 @@
+/*
+ * (C) Copyright 2018 Nexell
+ * JungHyun, Kim <jhkim@nexell.co.kr>
+ *
+ * SPDX-License-Identifier:      GPL-2.0+
+ */
+
+#ifndef __NXP3220_VTK_H__
+#define __NXP3220_VTK_H__
+
+#include "nxp3220_common.h"
+
+/* System memory Configuration */
+#define	CONFIG_SYS_TEXT_BASE	0x43C00000
+#define	CONFIG_SYS_INIT_SP_ADDR	CONFIG_SYS_TEXT_BASE
+#define CONFIG_SYS_MONITOR_BASE	CONFIG_SYS_TEXT_BASE
+#define CONFIG_SYS_SDRAM_BASE	0x40000000
+#define CONFIG_SYS_SDRAM_SIZE	0x10000000
+#define CONFIG_SYS_MALLOC_LEN	(64 * SZ_1M)
+
+/* kernel load address */
+#define CONFIG_SYS_LOAD_ADDR	0x48000000
+
+/* environments */
+#define CONFIG_EXTRA_ENV_SETTINGS \
+	"bootdelay=0\0"
+
+#endif
