13224d477e3bc442491ab755b98e5f82f17e184c
Min GyoungBo <mingyoungbo@nexell.co.kr>
2018-06-20 09:26:14 +0900

    pmic: sm5011: add pmic driver based on nxp3220
    This patch adds pmic driver based on nxp3220.
    
    Change-Id: Ie53699c5ccf871c5e5eb2da44571ffeab7a1a535
    Signed-off-by: Min GyoungBo <mingyoungbo@nexell.co.kr>

---
+++ drivers/power/pmic/sm5011.c
@@ -0,0 +1,179 @@
+/* SPDX-License-Identifier: (GPL-2.0+ or MIT) */
+/*
+ * PMIC driver for the SM5011
+ *
+ * (C) Copyright 2018 Nexell
+ * GyoungBo, Min <mingyoungbo@nexell.co.kr>
+ */
+
+#include <common.h>
+#include <fdtdec.h>
+#include <errno.h>
+#include <dm.h>
+#include <i2c.h>
+#include <power/pmic.h>
+#include <power/regulator.h>
+#include <power/sm5011.h>
+
+struct dm_sm5011_platdata {
+	int num_bucks;
+	int num_ldos;
+
+	int npwrstm_pin;
+	int irq_base;
+	int irq_gpio;
+	int entcxo_pin;
+	int enl16_pin;
+
+	bool wakeup;
+
+	/* Bcuk2 */
+	int buck2mode;
+
+	/* Bcuk3 */
+	int buck3mode;
+
+	/* Bcuk4 */
+	int buck4mode;
+
+	/* Bcuk5 */
+	int buck5mode;
+
+	/* Bcuk6 */
+	int buck6mode;
+
+	int en_32kout;
+	int smpl_en;
+	int smpl_power_on_type;
+	int smpl_timer_val;
+
+	int longkey_val;
+
+	int npwrstm_en;
+	int mask_int_en;
+
+	int envbatng_en;
+	int envref_en;
+
+	int mrstb_en;
+	int mrstb_hehavior;
+	int mrstb_nreset;
+	int mrstb_key;
+	int mrstb_timer_val;
+
+	int wdt_en;
+	int wdt_timer_val;
+
+	int comp_en;
+	int comp_time_val;
+	int comp_duty_val;
+	int comp_vref_val;
+
+	/* ---- RTC ---- */
+	int rtc_24hr_mode;
+	struct rtc_time *init_time;
+};
+
+static const struct pmic_child_info pmic_reg_info[] = {
+	{.prefix = "BUCK", .driver = SM5011_BUCK_DRIVER},
+	{.prefix = "LDO", .driver = SM5011_LDO_DRIVER},
+	{},
+};
+
+static int sm5011_reg_count(struct udevice *dev)
+{
+	return SM5011_NUM_OF_REGS;
+}
+
+static int sm5011_write(struct udevice *dev, uint reg, const uint8_t *buff,
+			int len)
+{
+	if (dm_i2c_write(dev, reg, buff, len)) {
+		pr_err("write error to device: %p register: %#x!", dev, reg);
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static int sm5011_read(struct udevice *dev, uint reg, uint8_t *buff, int len)
+{
+	if (dm_i2c_read(dev, reg, buff, len)) {
+		pr_err("read error from device: %p register: %#x!", dev, reg);
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static int sm5011_param_setup(struct udevice *dev, uint8_t *cache)
+{
+	return 0;
+}
+
+static int sm5011_device_setup(struct udevice *dev, uint8_t *cache)
+{
+	return 0;
+}
+
+static int sm5011_probe(struct udevice *dev)
+{
+	uint8_t value = 0x0;
+	uint8_t cache[256] = {
+		0x0,
+	};
+
+	debug("%s:%d\n", __func__, __LINE__);
+
+	sm5011_param_setup(dev, cache);
+	sm5011_device_setup(dev, cache);
+
+	sm5011_read(dev, SM5011_REG_DEVICEID, &value, 1);
+	printf("%s:%d: CHIP ID=0x%02x\n ", __func__, __LINE__, value);
+
+	return 0;
+}
+
+static int sm5011_bind(struct udevice *dev)
+{
+	ofnode regulators_node;
+	int children;
+
+	debug("%s: dev->name:%s\n", __func__, dev->name);
+
+	regulators_node = dev_read_subnode(dev, "voltage-regulators");
+
+	if (ofnode_valid(regulators_node)) {
+		debug("%s: found regulators subnode\n", __func__);
+		children = pmic_bind_children(dev, regulators_node,
+					      pmic_reg_info);
+		if (!children)
+			debug("%s: %s - no child found\n", __func__, dev->name);
+	} else {
+		debug("%s: regulators subnode not found!\n", __func__);
+	}
+
+	/* Always return success for this device */
+	return 0;
+}
+
+static struct dm_pmic_ops sm5011_ops = {
+	.reg_count = sm5011_reg_count,
+	.read = sm5011_read,
+	.write = sm5011_write,
+};
+
+static const struct udevice_id sm5011_ids[] = {
+	{ .compatible = "sm,sm5011", .data = (ulong)TYPE_SM5011 },
+	{}
+};
+
+U_BOOT_DRIVER(pmic_sm5011) = {
+	.name = "sm5011_pmic",
+	.id = UCLASS_PMIC,
+	.of_match = sm5011_ids,
+	.platdata_auto_alloc_size = sizeof(struct dm_sm5011_platdata),
+	.bind = sm5011_bind,
+	.probe = sm5011_probe,
+	.ops = &sm5011_ops,
+};
---
+++ drivers/power/regulator/sm5011.c
@@ -0,0 +1,435 @@
+/* SPDX-License-Identifier: (GPL-2.0+ or MIT) */
+/*
+ * Regulator driver for the SM5011
+ *
+ * (C) Copyright 2018 Nexell
+ * GyoungBo, Min <mingyoungbo@nexell.co.kr>
+ */
+
+#include <common.h>
+#include <fdtdec.h>
+#include <errno.h>
+#include <dm.h>
+#include <i2c.h>
+#include <power/pmic.h>
+#include <power/regulator.h>
+#include <power/sm5011.h>
+
+/**
+ * struct sm5011_para - sm5011 register parameters
+ * @param vol_addr		i2c address of the given buck/ldo register
+ * @param vol_bitpos	bit position to be set or clear within register
+ * @param vol_bitmask	bit mask value
+ * @param reg_enaddr	control register address, which enable the given
+ *						given buck/ldo.
+ * @param reg_enbiton	value to be written to buck/ldo to make it ON
+ * @param vol			Voltage information
+ */
+struct sm5011_para {
+	enum sm5011_regnum regnum;
+	u8 vol_addr;
+	u8 slp_vol_addr;
+	u8 vol_bitpos;
+	u8 vol_bitmask;
+	u8 reg_enaddr;
+	u8 reg_enbitpos;
+	const struct sec_voltage_desc *vol;
+};
+
+struct dm_sm5011_buck_platdata {
+	int always_onoff_npwrstm;
+	int always_lpm_npwrstm;
+	int init_uV;
+	unsigned always_on:1;	/* regulator never off when system is on */
+	unsigned boot_on:1;	/* bootloader/firmware enabled regulator */
+	unsigned on:1;	/* enabled regulator */
+};
+
+struct dm_sm5011_ldo_platdata {
+	int always_onoff_npwrstm;
+	int always_lpm_npwrstm;
+	int init_uV;
+	unsigned always_on:1;	/* regulator never off when system is on */
+	unsigned boot_on:1;	/* bootloader/firmware enabled regulator */
+	unsigned on:1;	/* enabled regulator */
+};
+
+static const struct sec_voltage_desc buck_v1 = {
+	.min = 562500,
+	.max = 2150000,
+	.step = 12500,
+};
+
+static const struct sec_voltage_desc ldo_v1 = {
+	.min = 800000,
+	.max = 3350000,
+	.step = 50000,
+};
+
+static const struct sm5011_para sm5011_buck_param[] = {
+	{0x00, 0x00, 0x00, 0x0, 0xff, 0x00, 0x00, &buck_v1},	/* dummy */
+	{0x00, 0x00, 0x00, 0x0, 0xff, 0x00, 0x00, &buck_v1},	/* dummy */
+	{SM5011_BUCK2, SM5011_REG_BUCK2CNTL2, 0x00, 0x0, 0xff,
+		SM5011_REG_BUCK2CNTL1, 0x01, &buck_v1},
+	{SM5011_BUCK3, SM5011_REG_BUCK3CNTL2, 0x00, 0x0, 0xff,
+		SM5011_REG_BUCK3CNTL1, 0x01, &buck_v1},
+	{SM5011_BUCK4, SM5011_REG_BUCK4CNTL2, 0x00, 0x0, 0xff,
+		SM5011_REG_BUCK4CNTL1, 0x01, &buck_v1},
+	{SM5011_BUCK5, SM5011_REG_BUCK5CNTL2, 0x00, 0x0, 0xff,
+		SM5011_REG_BUCK5CNTL1, 0x01, &buck_v1},
+	{SM5011_BUCK6, SM5011_REG_BUCK6CNTL2, 0x00, 0x0, 0xff,
+		SM5011_REG_BUCK6CNTL1, 0x01, &buck_v1},
+};
+
+static const struct sm5011_para sm5011_ldo_param[] = {
+	{0x00, 0x00, 0x00, 0x0, 0x00, 0x00, 0, &ldo_v1},	/* dummy */
+	{SM5011_LDO1, SM5011_REG_LDO1CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO1CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO2, SM5011_REG_LDO2CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO2CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO3, SM5011_REG_LDO3CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO3CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO4, SM5011_REG_LDO4CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO4CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO5, SM5011_REG_LDO5CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO5CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO6, SM5011_REG_LDO6CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO6CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO7, SM5011_REG_LDO7CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO7CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO8, SM5011_REG_LDO8CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO8CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO9, SM5011_REG_LDO9CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO9CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO10, SM5011_REG_LDO10CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO10CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO11, SM5011_REG_LDO11CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO11CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO12, SM5011_REG_LDO12CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO12CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO13, SM5011_REG_LDO13CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO13CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO14, SM5011_REG_LDO14CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO14CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO15, SM5011_REG_LDO15CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO15CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO16, SM5011_REG_LDO16CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO16CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO17, SM5011_REG_LDO17CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO17CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO18, SM5011_REG_LDO18CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO18CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO19, SM5011_REG_LDO19CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO19CNTL1, 0x01, &ldo_v1},
+	{SM5011_LDO20, SM5011_REG_LDO20CNTL2, 0x00, 0x00, 0x3f,
+		SM5011_REG_LDO20CNTL1, 0x01, &ldo_v1},
+};
+
+static int sm5011_reg_get_value(struct udevice *dev,
+				const struct sm5011_para *param)
+{
+	const struct sec_voltage_desc *desc;
+	int ret, uv, val;
+
+	ret = pmic_reg_read(dev->parent, param->vol_addr);
+	if (ret < 0)
+		return ret;
+
+	desc = param->vol;
+	val = (ret >> param->vol_bitpos) & param->vol_bitmask;
+	uv = desc->min + val * desc->step;
+
+	return uv;
+}
+
+static int sm5011_reg_set_value(struct udevice *dev,
+				const struct sm5011_para *param, int uv)
+{
+	const struct sec_voltage_desc *desc;
+	int ret, val;
+
+	desc = param->vol;
+	debug("%s:%d\n", __func__, __LINE__);
+	debug("min=%d, uv=%d, max=%d\n", desc->min, uv, desc->max);
+	if (uv < desc->min || uv > desc->max)
+		return -EINVAL;
+
+	val = (uv - desc->min) / desc->step;
+
+	debug("%s:%d val=0x%x\n", __func__, __LINE__, val);
+	val = (val & param->vol_bitmask) << param->vol_bitpos;
+
+	debug("%s:%d masked val=0x%x\n", __func__, __LINE__, val);
+	debug("addr=0x%x, bitmask=0x%x, bitpos=0x%x\n"
+	      , param->vol_addr, param->vol_bitmask, param->vol_bitpos);
+	ret = pmic_clrsetbits(dev->parent, param->vol_addr,
+			      param->vol_bitmask << param->vol_bitpos, val);
+
+	return ret;
+}
+
+static int sm5011_ldo_get_value(struct udevice *dev)
+{
+	const struct sm5011_para *ldo_param = NULL;
+	int ldo = dev->driver_data;
+
+	switch (dev_get_driver_data(dev_get_parent(dev))) {
+	case TYPE_SM5011:
+		ldo_param = &sm5011_ldo_param[ldo];
+		break;
+	default:
+		debug("Unsupported SM5011\n");
+		return -EINVAL;
+	}
+
+	return sm5011_reg_get_value(dev, ldo_param);
+}
+
+static int sm5011_ldo_set_value(struct udevice *dev, int uv)
+{
+	const struct sm5011_para *ldo_param = NULL;
+	int ldo = dev->driver_data;
+
+	switch (dev_get_driver_data(dev_get_parent(dev))) {
+	case TYPE_SM5011:
+		ldo_param = &sm5011_ldo_param[ldo];
+		break;
+	default:
+		debug("Unsupported SM5011\n");
+		return -EINVAL;
+	}
+	debug("%s:%d ldo=%d\n", __func__, __LINE__, ldo);
+
+	return sm5011_reg_set_value(dev, ldo_param, uv);
+}
+
+static int sm5011_reg_get_enable(struct udevice *dev,
+				 const struct sm5011_para *param)
+{
+	int ret;
+
+	ret = pmic_reg_read(dev->parent, param->reg_enaddr);
+	if (ret < 0)
+		return ret;
+
+	if ((ret >> param->reg_enbitpos) & 0x1)
+		return 0;
+	else
+		return -ENODEV;
+}
+
+static int sm5011_reg_set_enable(struct udevice *dev,
+				 const struct sm5011_para *param, bool enable)
+{
+	int ret;
+
+	ret = pmic_reg_read(dev->parent, param->reg_enaddr);
+	if (ret < 0)
+		return ret;
+
+	debug("%s:%d enable addr=0x%x\n", __func__, __LINE__
+	      , param->reg_enaddr);
+	debug("reg enable bitpos=0x%x\n", param->reg_enbitpos);
+
+	ret = pmic_clrsetbits(dev->parent, param->reg_enaddr,
+			      0x1 << param->reg_enbitpos,
+			      enable ? 0x1 << param->reg_enbitpos : 0);
+
+	return ret;
+}
+
+static int sm5011_ldo_get_enable(struct udevice *dev)
+{
+	const struct sm5011_para *ldo_param = NULL;
+	int ldo = dev->driver_data;
+
+	switch (dev_get_driver_data(dev_get_parent(dev))) {
+	case TYPE_SM5011:
+		ldo_param = &sm5011_ldo_param[ldo];
+		break;
+	default:
+		debug("Unsupported SM5011\n");
+		return -EINVAL;
+	}
+
+	return sm5011_reg_get_enable(dev, ldo_param);
+}
+
+static int sm5011_ldo_set_enable(struct udevice *dev, bool enable)
+{
+	const struct sm5011_para *ldo_param = NULL;
+	int ldo = dev->driver_data;
+
+	switch (dev_get_driver_data(dev_get_parent(dev))) {
+	case TYPE_SM5011:
+		ldo_param = &sm5011_ldo_param[ldo];
+		break;
+	default:
+		debug("Unsupported SM5011\n");
+		return -EINVAL;
+	}
+
+	return sm5011_reg_set_enable(dev, ldo_param, enable);
+}
+
+static int sm5011_ldo_probe(struct udevice *dev)
+{
+	struct dm_sm5011_buck_platdata *pdata = dev->platdata;
+	struct dm_regulator_uclass_platdata *uc_pdata;
+
+	uc_pdata = dev_get_uclass_platdata(dev);
+
+	uc_pdata->type = REGULATOR_TYPE_LDO;
+	uc_pdata->mode_count = 0;
+
+	if (!pdata->boot_on) {
+		if (pdata->init_uV != -ENODATA)
+			sm5011_ldo_set_value(dev, pdata->init_uV);
+
+		/* DCDC - Enable */
+		if (pdata->on == 1)
+			sm5011_ldo_set_enable(dev, 1);
+		else if (pdata->on == 0)
+			sm5011_ldo_set_enable(dev, 0);
+	}
+
+	return 0;
+}
+
+static int sm5011_ldo_ofdata_to_platdata(struct udevice *dev)
+{
+	struct dm_sm5011_ldo_platdata *pdata = dev->platdata;
+	debug("%s:%d\n", __func__, __LINE__);
+
+	pdata->init_uV = dev_read_u32_default(dev, "sm5011,init_uV", -ENODATA);
+	debug(" pdata->init_uV=%d\n", pdata->init_uV);
+
+	pdata->boot_on = dev_read_bool(dev, "regulator-boot-on");
+	debug(" pdata->boot_on=%d\n", pdata->boot_on);
+
+	pdata->always_on = dev_read_bool(dev, "regulator-always-on");
+	debug(" pdata->always_on=%d\n", pdata->always_on);
+
+	pdata->on = dev_read_u32_default(dev, "sm5011,on", -ENODATA);
+	debug(" pdata->on=%d\n", pdata->on);
+
+	return 0;
+}
+
+static int sm5011_ldo_bind(struct udevice *dev)
+{
+	return 0;
+}
+
+static int sm5011_buck_get_value(struct udevice *dev)
+{
+	int buck = dev->driver_data;
+
+	return sm5011_reg_get_value(dev, &sm5011_buck_param[buck]);
+}
+
+static int sm5011_buck_set_value(struct udevice *dev, int uv)
+{
+	int buck = dev->driver_data;
+
+	debug("%s:%d buck=%d\n", __func__, __LINE__, buck);
+
+	return sm5011_reg_set_value(dev, &sm5011_buck_param[buck], uv);
+}
+
+static int sm5011_buck_get_enable(struct udevice *dev)
+{
+	int buck = dev->driver_data;
+
+	return sm5011_reg_get_enable(dev, &sm5011_buck_param[buck]);
+}
+
+static int sm5011_buck_set_enable(struct udevice *dev, bool enable)
+{
+	int buck = dev->driver_data;
+
+	return sm5011_reg_set_enable(dev, &sm5011_buck_param[buck], enable);
+}
+
+static int sm5011_buck_probe(struct udevice *dev)
+{
+	struct dm_sm5011_buck_platdata *pdata = dev->platdata;
+	struct dm_regulator_uclass_platdata *uc_pdata;
+
+	uc_pdata = dev_get_uclass_platdata(dev);
+
+	uc_pdata->type = REGULATOR_TYPE_BUCK;
+	uc_pdata->mode_count = 0;
+
+	if (!pdata->boot_on) {
+		if (pdata->init_uV != -ENODATA)
+			sm5011_buck_set_value(dev, pdata->init_uV);
+
+		/* DCDC - Enable */
+		if (pdata->on == 1)
+			sm5011_buck_set_enable(dev, 1);
+		else if (pdata->on == 0)
+			sm5011_buck_set_enable(dev, 0);
+	}
+
+	return 0;
+}
+
+static int sm5011_buck_ofdata_to_platdata(struct udevice *dev)
+{
+	struct dm_sm5011_buck_platdata *pdata = dev->platdata;
+	debug("%s:%d\n", __func__, __LINE__);
+
+	pdata->init_uV = dev_read_u32_default(dev, "sm5011,init_uV", -ENODATA);
+	debug(" pdata->init_uV=%d\n", pdata->init_uV);
+
+	pdata->boot_on = dev_read_bool(dev, "regulator-boot-on");
+	debug(" pdata->boot_on=%d\n", pdata->boot_on);
+
+	pdata->always_on = dev_read_bool(dev, "regulator-always-on");
+	debug(" pdata->always_on=%d\n", pdata->always_on);
+
+	pdata->on = dev_read_u32_default(dev, "sm5011,on", -ENODATA);
+	debug(" pdata->on=%d\n", pdata->on);
+
+	return 0;
+}
+
+static int sm5011_buck_bind(struct udevice *dev)
+{
+	return 0;
+}
+
+static const struct dm_regulator_ops sm5011_ldo_ops = {
+	.get_value  = sm5011_ldo_get_value,
+	.set_value  = sm5011_ldo_set_value,
+	.get_enable = sm5011_ldo_get_enable,
+	.set_enable = sm5011_ldo_set_enable,
+};
+
+U_BOOT_DRIVER(sm5011_ldo) = {
+	.name = SM5011_LDO_DRIVER,
+	.id = UCLASS_REGULATOR,
+	.ops = &sm5011_ldo_ops,
+	.bind = sm5011_ldo_bind,
+	.probe = sm5011_ldo_probe,
+	.ofdata_to_platdata = sm5011_ldo_ofdata_to_platdata,
+	.platdata_auto_alloc_size = sizeof(struct dm_sm5011_ldo_platdata),
+};
+
+static const struct dm_regulator_ops sm5011_buck_ops = {
+	.get_value  = sm5011_buck_get_value,
+	.set_value  = sm5011_buck_set_value,
+	.get_enable = sm5011_buck_get_enable,
+	.set_enable = sm5011_buck_set_enable,
+};
+
+U_BOOT_DRIVER(sm5011_buck) = {
+	.name = SM5011_BUCK_DRIVER,
+	.id = UCLASS_REGULATOR,
+	.ops = &sm5011_buck_ops,
+	.bind = sm5011_buck_bind,
+	.probe = sm5011_buck_probe,
+	.ofdata_to_platdata = sm5011_buck_ofdata_to_platdata,
+	.platdata_auto_alloc_size = sizeof(struct dm_sm5011_buck_platdata),
+};
