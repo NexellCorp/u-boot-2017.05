ebfffc0f37e9d7b7a86b4dfc97826cd76826cdb8
Min GyoungBo <mingyoungbo@nexell.co.kr>
2018-06-22 21:50:52 +0900

    board: nexell: add sm5011 pmic
    This patch adds sm5011 pmic on nxp3220_vtk board.
    
    Change-Id: I02d05d679aa950d531e0bb184874c630402f5def
    Signed-off-by: Min GyoungBo <mingyoungbo@nexell.co.kr>

--- board/nexell/nxp3220_vtk/board.c
+++ board/nexell/nxp3220_vtk/board.c
@@ -8,9 +8,38 @@
 #include <config.h>
 #include <common.h>
 
+#ifdef CONFIG_DM_PMIC_SM5011
+#include <power/pmic.h>
+#include <power/regulator.h>
+#endif
+
 DECLARE_GLOBAL_DATA_PTR;
 
 int board_init(void)
 {
 	return 0;
 }
+
+#ifdef CONFIG_DM_PMIC_SM5011
+int power_init_board(void)
+{
+	struct udevice *pmic;
+	int ret = -ENODEV;
+
+	debug("%s:%d\n", __func__, __LINE__);
+
+	ret = pmic_get("sm5011@47", &pmic);
+	if (ret)
+		printf("Can't get PMIC: %s!\n", "sm5011@47");
+
+	debug("%s:%d, regulators_enable_boot_on() start\n", __func__, __LINE__);
+
+	ret = regulators_enable_boot_on(false);
+	if (ret)
+		return ret;
+	debug("%s:%d, regulators_enable_boot_on() end\n", __func__, __LINE__);
+
+	return 0;
+}
+#endif
+
