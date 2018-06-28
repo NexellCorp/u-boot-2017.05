f5ad71efa180c3ceff41f1f6636f33a67804339e
Chanho Park <chanho61.park@samsung.com>
2018-06-28 20:59:03 +0900

    board: nexell: nxp3220-vtk: set sdboot environment
    This patch supports to set the "mmc_boot_dev" environment variable
    if current boot mode is the sd boot mode. By default, the value is 0
    which is indicated eMMC boot. The value will be used to provide a mmc
    boot device number.
    
    Change-Id: Iaa882a51ecfd7075a1d43aeb6c09f8b9d5e9cc53
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- board/nexell/nxp3220_vtk/board.c
+++ board/nexell/nxp3220_vtk/board.c
@@ -7,18 +7,48 @@
 
 #include <config.h>
 #include <common.h>
+#include <asm/io.h>
 
 #ifdef CONFIG_DM_PMIC_SM5011
 #include <power/pmic.h>
 #include <power/regulator.h>
 #endif
 
+#define REG_RST_CONFIG	0x2008c86c
+#define BOOTMODE_MASK	0x7
+#define BOOTMODE_SDMMC	0x3
+
 DECLARE_GLOBAL_DATA_PTR;
+
+#ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
+void set_board_info(void)
+{
+	u32 rst_config;
+
+	rst_config = readl(REG_RST_CONFIG);
+	rst_config &= BOOTMODE_MASK;
+
+	/* set boot device only if it was sdmmc boot */
+	if (rst_config == BOOTMODE_SDMMC)
+		env_set_ulong("mmc_boot_dev", 1);
+}
+#endif
 
 int board_init(void)
 {
 	return 0;
 }
+
+#ifdef CONFIG_MISC_INIT_R
+int misc_init_r(void)
+{
+#ifdef CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
+	set_board_info();
+#endif
+
+	return 0;
+}
+#endif
 
 #ifdef CONFIG_DM_PMIC_SM5011
 int power_init_board(void)
@@ -42,4 +72,3 @@ int power_init_board(void)
 	return 0;
 }
 #endif
-
