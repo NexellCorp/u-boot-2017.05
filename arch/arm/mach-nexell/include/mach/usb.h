fb748f9d822b5b749308621117810a349d9448b1
jhkim <jhkim@nexell.co.kr>
2018-08-03 16:07:09 +0900

    usb: nxp3220 usb otg phy: remove usb otg phy custom driver
    This patch remove usb otg phy custom driver, replaced generic phy driver
    
    Change-Id: I467d6aebfb7cbc79e8e682cfec1b8d27a5d00979
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

--- arch/arm/mach-nexell/include/mach/usb.h
+++ arch/arm/mach-nexell/include/mach/usb.h
@@ -7,14 +7,6 @@
 #ifndef __NEXELL_USB_H
 #define __NEXELL_USB_H
 
-struct nx_otg_phy {
-	struct udevice *dev;
-	void __iomem *addr;
-};
-
-void nx_otg_phy_init(struct nx_otg_phy *phy);
-void nx_otg_phy_off(struct nx_otg_phy *phy);
-
 int nx_usb_download(ulong buffer);
 
 #endif
