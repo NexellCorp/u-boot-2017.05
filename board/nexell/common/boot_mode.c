9d2c585dfa884a0ea7b2800f5febc725997b21e6
Chanho Park <chanho61.park@samsung.com>
2018-09-28 15:35:13 +0900

    board: nexell: boot_mode: support fastboot reboot-bootloader
    This patch adds to support reboot-bootloader command while entering
    fastboot mode.
    
    Change-Id: I8f04f54c30cfa11f892dab9aade0d45cd2675515
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- board/nexell/common/boot_mode.c
+++ board/nexell/common/boot_mode.c
@@ -11,42 +11,56 @@
 #include <linux/err.h>
 #include "boot_mode.h"
 
-int check_boot_mode(void)
+static struct regmap *get_boot_mode_regmap(unsigned int *offset)
 {
 	int node, ret;
 	struct fdtdec_phandle_args args;
 	struct udevice *syscon;
 	struct regmap *regmap;
-	unsigned int offset;
-	unsigned int boot_mode;
 
 	node = fdt_node_offset_by_compatible(gd->fdt_blob, 0,
 					     "nexell,boot-mode");
 	if (node < 0)
-		return -ENODEV;
+		return NULL;
 
 	ret = fdtdec_parse_phandle_with_args(gd->fdt_blob, node, "syscon",
 					     NULL, 0, 0, &args);
 	if (ret) {
 		pr_err("%s: Cannot get syscon phandle: ret=%d\n", __func__,
 		       ret);
-		return -ENODEV;
+		return NULL;
 	}
 
 	ret = uclass_get_device_by_of_offset(UCLASS_SYSCON, args.node,
 					     &syscon);
 	if (ret) {
 		pr_err("%s: Cannot get SYSCON: ret=%d\n", __func__, ret);
-		return -ENODEV;
+		return NULL;
 	}
 
 	regmap = syscon_get_regmap(syscon);
 	if (!regmap) {
 		pr_err("unable to find regmap\n");
-		return -ENODEV;
+		return NULL;
 	}
 
-	offset = ofnode_read_u32_default(offset_to_ofnode(node), "offset", 0);
+	*offset = ofnode_read_u32_default(offset_to_ofnode(node), "offset", 0);
+
+	return regmap;
+}
+
+int check_boot_mode(void)
+{
+	int ret;
+	struct regmap *regmap;
+	unsigned int offset;
+	unsigned int boot_mode;
+
+	regmap = get_boot_mode_regmap(&offset);
+	if (!regmap) {
+		pr_err("unable to retrieve regmap\n");
+		return -ENODEV;
+	}
 
 	ret = regmap_read(regmap, offset, &boot_mode);
 	if (ret) {
@@ -72,3 +86,18 @@ int check_boot_mode(void)
 	return 0;
 }
 
+int fb_set_reboot_flag(void)
+{
+	struct regmap *regmap;
+	unsigned int offset;
+
+	regmap = get_boot_mode_regmap(&offset);
+	if (!regmap) {
+		pr_err("unable to retrieve regmap\n");
+		return -ENODEV;
+	}
+
+	regmap_write(regmap, offset, BOOT_FASTBOOT);
+
+	return 0;
+}
