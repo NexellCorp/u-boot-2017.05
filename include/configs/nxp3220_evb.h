9baed9e7cb6559a9521430804abbe48f9f1f3725
jhkim <jhkim@nexell.co.kr>
2018-11-19 16:04:29 +0900

    config: nxp3220 evb: add format_emmc environment to set emmc bootmode
    This patch add format_emmc environment to set emmc bootmode
    
    - mmc bootbus 0 2 0 0
    	: change emmc boot mode bits (1:4bit/2:8bit)
    
    	mmc bootbus <dev> <boot_bus_width> <reset_boot_bus_width> <boot_mode>
    
    	boot_bus_width :  boot bus widtth sel
    	0x0 : x1 (sdr) or x4 (ddr) bus width in boot operation mode (default)
    	0x1 : x4 (sdr/ddr) bus width in boot operation mode
    	0x2 : x8 (sdr/ddr) bus width in boot operation mode
    	0x3 : Reserved
    
    	reset_boot_bus_width : reset boot bus confitions
    	0x0 : Reset bus width to x1, single data rate and backward compatible timings after boot operation
    	0x1 : Retain BOOT_BUS_WIDTH and BOOT_MODE values after boot operation. This is relevant to Push-pull mode operation only
    
    	boot_mode :  set boot mode
    	0x0 : Use single data rate + backward compatible timings in boot operation (default)
    	0x1 : Use single data rate + High Speed timings in boot operation mode
    	0x2 : Use dual data rate in boot operation
    	0x3 : Reserved
    
    - mmc rst-function 0 1
    	: adjust emmc hw reset
    
    Change-Id: If575a0af8ba965a633d094becfd2681e3dcd9eae
    Signed-off-by: jhkim <jhkim@nexell.co.kr>

--- include/configs/nxp3220_evb.h
+++ include/configs/nxp3220_evb.h
@@ -32,6 +32,7 @@
 #define MMC_BOOT_PART_TYPE	"ext4"
 #define MMC_ROOTFS_PART_TYPE	"ext4"
 
+#define	KERNEL_DTB		"nxp3220-evb.dtb"
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"autoboot=run boot_rootfs\0" \
@@ -43,7 +44,7 @@
 		"bootz ${kernel_addr} - ${fdt_addr}\0" \
 	"console=" CONFIG_DEFAULT_CONSOLE \
 	"fdt_addr=" __stringify(FDT_ADDR) "\0" \
-	"fdt_file=nxp3220-evb.dtb\0" \
+	"fdt_file="__stringify(KERNEL_DTB) "\0" \
 	"load_args=setenv bootargs \"" \
 		"root=/dev/mmcblk${mmc_boot_dev}p${mmc_rootfs_part} " \
 		"rootfstype=${mmc_rootfs_part_type} ${root_rw} " \
@@ -53,7 +54,7 @@
 		"${kernel_file}\0" \
 	"load_fdt=ext4load mmc ${mmc_boot_dev}:${mmc_boot_part} ${fdt_addr} " \
 		"${fdt_file}\0" \
-	"log_msg=loglevel=7 printk.time=1 earlyprintk\0" \
+	"log_msg=loglevel=7 printk.time=1\0" \
 	"kernel_addr=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
 	"kernel_file=zImage\0" \
 	"mmc_boot_dev=" __stringify(MMC_BOOT_DEV) "\0" \
@@ -62,5 +63,10 @@
 	"mmc_rootfs_part=" __stringify(MMC_ROOTFS_PART) "\0" \
 	"mmc_rootfs_part_type=" MMC_ROOTFS_PART_TYPE "\0" \
 	"root_rw=rw\0" \
+	"format_emmc=" \
+		"mmc rescan;" \
+		"mmc bootbus 0 2 0 0;" \
+		"mmc rst-function 0 1;" \
+		"mmc rescan\0" \
 
 #endif
