14794d25bd043440a7bb913306b75069e0ce9a1d
Lee Geunjeong <kjlee@nexell.co.kr>
2019-01-22 18:55:06 +0900

    config: nxp3220 evb: change bootargs to disable kernel/systemd console message
    This patch change bootargs to disable kernel/systemd console message
    - disable kernel boot message  : "quiet"
    - disable systemd boot message : "systemd.log_level=info systemd.show_status=false"
    
    Signed-off-by: Lee Geunjeong <kjlee@nexell.co.kr>
    Change-Id: I0bfbe7dd9e05526faadbbe7a113f7ddc1397ffa3

--- include/configs/nxp3220_evb.h
+++ include/configs/nxp3220_evb.h
@@ -36,35 +36,36 @@
 #define MMC_ROOTFS_PART_TYPE	"ext4"
 
 #define	KERNEL_DTB		"nxp3220-evb.dtb"
+#define LOG_STATUS		"quiet systemd.log_level=info systemd.show_status=false"
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	"autoboot=run boot_rootfs\0" \
-	"bootdelay=" __stringify(CONFIG_BOOTDELAY) "\0" \
+	"bootdelay="__stringify(CONFIG_BOOTDELAY) "\0" \
 	"boot_rootfs=" \
 		"run load_kernel;" \
 		"run load_fdt;" \
 		"run load_args;" \
 		"bootz ${kernel_addr} - ${fdt_addr}\0" \
-	"console=" CONFIG_DEFAULT_CONSOLE \
-	"fdt_addr=" __stringify(FDT_ADDR) "\0" \
+	"console="CONFIG_DEFAULT_CONSOLE \
+	"fdt_addr="__stringify(FDT_ADDR) "\0" \
 	"fdt_file="__stringify(KERNEL_DTB) "\0" \
 	"load_args=setenv bootargs \"" \
 		"root=/dev/mmcblk${mmc_boot_dev}p${mmc_rootfs_part} " \
 		"rootfstype=${mmc_rootfs_part_type} ${root_rw} " \
-		"${console} ${log_msg} ${opts}" \
+		"${console} ${log_msg} ${opt_log} ${opts}" \
 		"\"\0" \
 	"load_kernel=ext4load mmc ${mmc_boot_dev}:${mmc_boot_part} ${kernel_addr} " \
 		"${kernel_file}\0" \
 	"load_fdt=ext4load mmc ${mmc_boot_dev}:${mmc_boot_part} ${fdt_addr} " \
 		"${fdt_file}\0" \
 	"log_msg=loglevel=7 printk.time=1\0" \
-	"kernel_addr=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
+	"kernel_addr="__stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
 	"kernel_file=zImage\0" \
-	"mmc_boot_dev=" __stringify(MMC_BOOT_DEV) "\0" \
-	"mmc_boot_part=" __stringify(MMC_BOOT_PART) "\0" \
-	"mmc_boot_part_type=" MMC_BOOT_PART_TYPE "\0" \
-	"mmc_rootfs_part=" __stringify(MMC_ROOTFS_PART) "\0" \
-	"mmc_rootfs_part_type=" MMC_ROOTFS_PART_TYPE "\0" \
+	"mmc_boot_dev="__stringify(MMC_BOOT_DEV) "\0" \
+	"mmc_boot_part="__stringify(MMC_BOOT_PART) "\0" \
+	"mmc_boot_part_type="MMC_BOOT_PART_TYPE "\0" \
+	"mmc_rootfs_part="__stringify(MMC_ROOTFS_PART) "\0" \
+	"mmc_rootfs_part_type="MMC_ROOTFS_PART_TYPE "\0" \
 	"root_rw=rw\0" \
 
 #endif
