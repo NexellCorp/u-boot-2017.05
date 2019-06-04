c0fce5cdff1d2f901db446e584a9a9084ae00687
jhkim <jhkim@nexell.co.kr>
2019-06-04 09:26:01 +0900

    nand: nexell: code cleanup
    This patch code cleanup for nxp3220 nand driver
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: Ib886aa23080013a97795bc21ffeb149304dee340
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/u-boot-2017.05/+/456709

--- drivers/mtd/nand/nxp3220_nand.h
+++ drivers/mtd/nand/nxp3220_nand.h
@@ -62,6 +62,7 @@ struct nxp3220_nfc {
 	struct clk core_clk;
 	struct gpio_desc wp_gpio;
 	int datasize;			/* boot image */
+	int eccbyte;			/* boot image */
 	int steps;			/* boot image */
 
 	struct nfc_cmd_time time;	/* calculated values */
@@ -110,20 +111,25 @@ enum NX_NANDC_BCH {
 	NX_NANDC_BCH_1024_60 = 10
 };
 
-enum NX_RESERVED_BLOCK_MODE {
-	MODE_OFF = 0,
-	MODE_ECC = 1,
-	MODE_RAW = 2
+enum NX_ECC_MANAGE {
+	ECC_MANAGE_OFF = 0,
+	ECC_MANAGE_BLD = 1, /* boot loader */
+	ECC_MANAGE_RAW = 2
 };
 
-int nand_hw_ecc_write_block(struct nand_chip *chip, const u8 *buf);
-int nand_hw_ecc_read_block(struct nand_chip *chip, u8 *buf);
+/* inband ecc, not use oob : data + ecc - data+ ecc .... */
+int nand_hw_ecc_write_bloader(struct mtd_info *mtd, struct nand_chip *chip,
+			      const u8 *buf, int page);
+int nand_hw_ecc_read_bloader(struct mtd_info *mtd, struct nand_chip *chip,
+			     u8 *buf, int page);
 
-int get_datasize(struct nand_chip *chip);
-int get_eccsteps(struct nand_chip *chip);
+int get_nand_chip_datasize(struct nand_chip *chip);
+int get_nand_chip_eccbyte(struct nand_chip *chip);
+int get_nand_chip_eccsteps(struct nand_chip *chip);
 
-int get_nand_rsvblk_mode(void);
-void set_nand_rsvblk_ecc(void);
-void set_nand_rsvblk_raw(void);
-void set_nand_rsvblk_off(void);
+int get_nand_chip_ecc_manage(void);
+void set_nand_chip_ecc_manage_bld(void);
+void set_nand_chip_ecc_manage_raw(void);
+void set_nand_chip_ecc_manage_off(void);
+
 #endif /* __NXP3220_NAND_H__ */
