e9fb5fe0f722e56088a4c56927ac02f4673858c5
jhkim <jhkim@nexell.co.kr>
2019-06-21 10:15:25 +0900

    board: nxp3220: support EVB2/DAUDIO2 board based on NXP3220 SoC
    This patch support EVB2/DAUDIO2 board based on NXP3220 SoC
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: I3e4ebaa32f6b006a27ed12d8725f956f828c456a
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/u-boot-2017.05/+/458900

--- board/nexell/nxp3220_evb/board.c
+++ board/nexell/nxp3220_evb/board.c
@@ -8,6 +8,12 @@
 #include <config.h>
 #include <common.h>
 #include <asm/io.h>
+#ifdef CONFIG_CHECK_REVISION
+#include <adc.h>
+#endif
+#ifdef CONFIG_DM_REGULATOR
+#include <power/regulator.h>
+#endif
 #include "../common/common.h"
 
 DECLARE_GLOBAL_DATA_PTR;
@@ -32,13 +38,64 @@ int misc_init_r(void)
 }
 #endif
 
+#ifdef CONFIG_CHECK_REVISION
+static int board_check_revision(unsigned int revision[], int size)
+{
+	unsigned int ch[2] = { 1, 2 };
+	unsigned int value;
+	int i, ret;
+
+	for (i = 0; i < 2; i++) {
+		ret = adc_channel_single_shot("adc@20600000", ch[i], &value);
+		if (ret) {
+			debug("Cannot read ADC.%d value\n", ch[i]);
+			return -EINVAL;
+		}
+
+		if (value < 227) /* 0 ~ 0.1v */
+			value = 0;
+		else
+			value = 1;
+
+		if (i < size)
+			revision[i] = value;
+	}
+
+	return ret;
+}
+#endif
+
 #ifdef CONFIG_BOARD_LATE_INIT
 int board_late_init(void)
 {
+#ifdef CONFIG_CHECK_REVISION
+	unsigned int rev[2] = { 0, };
+	int ret;
+
+	ret = board_check_revision(rev, 2);
+#ifndef CONFIG_QUICKBOOT_QUIET
+	if (!ret)
+		printf("REV: %d.%d\n", rev[0], rev[1]);
+#endif
+#endif
+
 #ifdef CONFIG_CMD_BMP
 	draw_logo();
 #endif
 	run_fastboot_update();
+	return 0;
+}
+#endif
+
+#ifdef CONFIG_DM_REGULATOR
+int power_init_board(void)
+{
+	int ret = -ENODEV;
+
+	ret = regulators_enable_boot_on(false);
+	if (ret)
+		return ret;
+
 	return 0;
 }
 #endif
