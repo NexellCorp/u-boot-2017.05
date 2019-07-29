e0e4be9466e72466019ea36d59954aca4677c9ee
jhkim <jhkim@nexell.co.kr>
2019-07-29 12:53:35 +0900

    clk: nxp3220: modify debug messages
    This patch modify debug messages
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: Ie917841e6314ba6e11c7fab2bb710bce362f161a

--- drivers/clk/nexell/clk-nxp3220.c
+++ drivers/clk/nexell/clk-nxp3220.c
@@ -20,6 +20,7 @@ DECLARE_GLOBAL_DATA_PTR;
 #define SYS_MAX_DIV	256
 #define T_MAX_DIV	(SRC_MAX_DIV * SYS_MAX_DIV)
 #define PLL_NUM		5
+#define	debug	printf
 
 struct nx_cmu_priv {
 	struct clk_cmu_dev *cmus;
@@ -152,6 +153,9 @@ static void nx_clk_calc_divider(struct nx_clk_div *cdiv, unsigned long request)
 			break;
 		}
 
+		debug("CAL: %luhz, %8d (%8d) [%d]=%lu/%d\n",
+		      request, delta, m, i, plls[i].value, v);
+
 		if (!delta || m < delta) {
 			select = i;
 			divide = v;
@@ -233,7 +237,7 @@ static int nx_clk_enable_hw(struct nx_cmu_priv *priv, unsigned int id)
 
 	writel(1 << bit, &cmu->reg->enb_set[idx]);
 
-	debug("%s:%3d:%p, idx:%d, bit:%d enbable\n",
+	debug("ON : %s:%3d:%p, idx:%d, bit:%d enbable\n",
 	      get_cmu_type(cmu->cmu_id), cmu->id, cmu->reg, idx, bit);
 
 	if (cmu->type == CMU_TYPE_MAINDIV) {
@@ -245,7 +249,7 @@ static int nx_clk_enable_hw(struct nx_cmu_priv *priv, unsigned int id)
 		bit = cmu->clkenbit % 32;
 		writel(1 << bit, &cmu->reg->enb_set[idx]);
 
-		debug("%s:%3d:%p, idx:%d, bit:%d enbable\n",
+		debug("ON : %s:%3d:%p, idx:%d, bit:%d enbable\n",
 		      get_cmu_type(cmu->cmu_id), cmu->id, cmu->reg, idx, bit);
 	}
 
@@ -271,7 +275,7 @@ static int nx_clk_disable(struct clk *clk)
 
 	writel(1 << bit, &cmu->reg->enb_clr[idx]);
 
-	debug("%s:%3d:%p, idx:%d, bit:%d disable\n",
+	debug("OFF: %s:%3d:%p, idx:%d, bit:%d disable\n",
 	      get_cmu_type(cmu->cmu_id), cmu->id, cmu->reg, idx, bit);
 
 	if (cmu->type == CMU_TYPE_MAINDIV) {
@@ -283,7 +287,7 @@ static int nx_clk_disable(struct clk *clk)
 		bit = cmu->clkenbit % 32;
 		writel(1 << bit, &cmu->reg->enb_clr[idx]);
 
-		debug("%s:%3d:%p, idx:%d, bit:%d disable\n",
+		debug("OFF: %s:%3d:%p, idx:%d, bit:%d disable\n",
 		      get_cmu_type(cmu->cmu_id), cmu->id, cmu->reg, idx, bit);
 	}
 
@@ -304,7 +308,7 @@ static ulong nx_clk_set_hw(struct clk_cmu_dev *src,
 
 	sys->freq = plls[cdiv.mux].value / cdiv.parent / cdiv.divide;
 
-	debug("%s:%3d:%p, %s:%3d:%p mux:%d, div:%d/%d, freq:%ld\n",
+	debug("SET: %s:%3d:%p, %s:%3d:%p mux:%d, div:%d/%d, freq:%ld\n",
 	      get_cmu_type(src->cmu_id), src->id, src->reg,
 	      get_cmu_type(sys->cmu_id), sys->id, sys->reg,
 	      cdiv.mux, cdiv.divide, cdiv.parent, freq);
