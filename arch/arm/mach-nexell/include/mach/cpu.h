214457d7f2f70b0c63127c29e53507a4230f0eb9
jhkim <jhkim@nexell.co.kr>
2019-07-30 15:47:55 +0900

    nxp3220: boot: speed up ddr3 memory initialize.
    This patch speed up the ddr3 memory initialize from save bit cal data to header.
    bit cal data will save vddpwr->new_scratch[7:14].
    u-boot must update that to bl2 header.
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: Ifb57a6aaf079a2afa98069e9eea9835a50bd03f6

---
+++ arch/arm/mach-nexell/include/mach/cpu.h
@@ -0,0 +1,22 @@
+/*
+ * (C) Copyright 2018 Nexell
+ * JungHyun, Kim <jhkim@nexell.co.kr>
+ *
+ * SPDX-License-Identifier: GPL-2.0+
+ */
+#ifndef __NEXELL_CPU_H
+#define __NEXELL_CPU_H
+
+enum boot_device {
+	BOOT_DEV_EMMC,
+	BOOT_DEV_USB,
+	BOOT_DEV_SD,
+	BOOT_DEV_NAND,
+	BOOT_DEV_SPI,
+	BOOT_DEV_UART,
+};
+
+int boot_check_mode(void);
+enum boot_device boot_current_device(void);
+
+#endif
