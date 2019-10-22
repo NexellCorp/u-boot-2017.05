d51b1740808cfa5566547e4d2aa1909047488ff5
jhkim <jhkim@nexell.co.kr>
2019-10-22 14:32:47 +0900

    armv7: nxp3220: adds reboot mode functions
    This patch adds reboot mode functions
    - enabel CONFIG_REBOOT_MODE
    - recovery mode   : run 'recovery-boot'
    - bootloader mode : run 'fastboot 0'
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: I17185b7c8d294b423c939554a31a92fc3089d5ae

--- arch/arm/mach-nexell/include/mach/cpu.h
+++ arch/arm/mach-nexell/include/mach/cpu.h
@@ -19,4 +19,13 @@ enum boot_device {
 int boot_check_bootup_mode(void);
 enum boot_device boot_current_device(void);
 
+/* high 24 bits(0x6e7870) is tag, low 8 bits is type of reboot */
+#define REBOOT_MAGIC		0x6e787000
+#define BOOT_NORMAL		(REBOOT_MAGIC + 0)	/* normal boot */
+#define BOOT_FASTBOOT		(REBOOT_MAGIC + 1)	/* fastboot mode */
+#define BOOT_RECOVERY		(REBOOT_MAGIC + 2)	/* recovery mode */
+#define BOOT_DFU		(REBOOT_MAGIC + 3)	/* dfu downloader mode */
+
+unsigned int boot_check_reboot_mode(void);
+
 #endif
