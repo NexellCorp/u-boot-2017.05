55e5d200a89061a57da96b501da698fcda048e53
jhkim <jhkim@nexell.co.kr>
2019-12-11 14:39:25 +0900

    defconfig: nxp3220_evb: Fix build error when set 'CONFIG_ENV_IS_NOWHERE'
    This patch fix build error when set 'CONFIG_ENV_IS_NOWHERE'
    
    Signed-off-by: jhkim <jhkim@nexell.co.kr>
    Change-Id: I6d7534e56a75add031119914380513239ff6a76a
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/u-boot-2017.05/+/477521

--- include/configs/nxp3220_daudio.h
+++ include/configs/nxp3220_daudio.h
@@ -49,8 +49,10 @@
 #define CONFIG_BMP_24BPP
 #define CONFIG_BMP_32BPP
 
-/* For SD/MMC */
 #if defined(CONFIG_ENV_IS_IN_MMC)
+/*
+ * For SD/MMC Environment
+ */
 #if defined(CONFIG_TARGET_NXP3220_DAUDIO)
 #define	ENV_KERNEL_DTB		"nxp3220-daudio.dtb"
 #define ENV_DAUDIO_OPTS		""
@@ -79,7 +81,7 @@
 #define ENV_MMC_ROOTFS_PART		3
 #define ENV_MMC_BOOT_PART_TYPE		"ext4"
 #define ENV_MMC_ROOTFS_PART_TYPE	"ext4"
-#define ENV_BOOT_PRECOMMNAD	""
+#define ENV_BOOT_PRECOMMAND	""
 #define ENV_BOOT_POSTCOMMAND	"bootl ${kernel_addr} - ${fdt_addr}"
 #define ENV_BOOT_ARGS		"root=/dev/mmcblk${mmc_root_dev}p${mmc_rootfs_part} " \
 				"rootfstype=${mmc_rootfs_part_type} ${root_rw} "
@@ -93,8 +95,10 @@
 				"mmc_rootfs_part_type="ENV_MMC_ROOTFS_PART_TYPE "\0"
 #define ENV_LOAD_INITRD_FILE	"load_initrd=ext4load mmc ${mmc_boot_dev}:${mmc_boot_part} ${initrd_addr} ${initrd_file}"
 
-/* For NAND Environment */
 #elif defined(CONFIG_ENV_IS_IN_NAND)
+/*
+ * For NAND Environment
+ */
 #if defined(CONFIG_TARGET_NXP3220_DAUDIO)
 #define	ENV_KERNEL_DTB		"nxp3220-daudio-nand.dtb"
 #define ENV_DAUDIO_OPTS		""
@@ -116,7 +120,7 @@
 					""ENV_NAND_BOOT_PART_SIZE"(boot),"\
 					""ENV_NAND_MISC_PART_SIZE"(misc),"\
 					"-(rootfs)"
-#define ENV_BOOT_PRECOMMNAD	"ubifsmount ubi0:boot; " /* ubi part boot;ubifsmount ubi0:boot; */
+#define ENV_BOOT_PRECOMMAND	"ubifsmount ubi0:boot; " /* ubi part boot;ubifsmount ubi0:boot; */
 #define ENV_BOOT_POSTCOMMAND	"bootl ${kernel_addr} - ${fdt_addr}"
 #define ENV_BOOT_ARGS		"ubi.mtd="__stringify(ENV_NAND_ROOT_PART)" " \
 				"ubi.mtd="__stringify(ENV_NAND_BOOT_PART)" " \
@@ -130,7 +134,19 @@
 				"ubiroot=ubi0:rootfs\0"
 #define ENV_LOAD_INITRD_FILE	"load_initrd=ubifsload ${initrd_addr} ${initrd_file}"
 
-#endif /* CONFIG_ENV_IS_IN_NAND */
+#else /* CONFIG_ENV_IS_IN_NAND */
+/*
+ * Environment is not stored
+ */
+#define ENV_BOOT_PRECOMMAND	""
+#define ENV_BOOT_POSTCOMMAND	""
+#define ENV_BOOT_ARGS		""
+#define ENV_LOAD_KERNEL_FILE	""
+#define ENV_LOAD_KERNEL_FDT	""
+#define ENV_EXTRA_SETTINGS	""
+#define ENV_LOAD_INITRD_FILE	""
+#define ENV_DAUDIO_OPTS		""
+#endif /* CONFIG_ENV_IS_NOWHERE */
 
 #define ENV_KERNEL_FILE		"Image"
 #define ENV_LOG_MSG		"quiet loglevel=3 printk.time=1"
@@ -145,7 +161,7 @@
 	"autoboot=run boot_rootfs\0" \
 	"bootdelay="__stringify(CONFIG_BOOTDELAY) "\0" \
 	"boot_rootfs=" \
-		ENV_BOOT_PRECOMMNAD \
+		ENV_BOOT_PRECOMMAND \
 		"run load_kernel;" \
 		"run load_fdt;" \
 		"run load_args;" \
@@ -171,7 +187,7 @@
 	"mem_resv="ENV_SPLASH_MEM_RESERVE "\0" \
 	ENV_EXTRA_SETTINGS \
 	"recovery-boot=" \
-		ENV_BOOT_PRECOMMNAD \
+		ENV_BOOT_PRECOMMAND \
 		"run load_kernel;" \
 		"run load_fdt;" \
 		"run load_initrd;" \
